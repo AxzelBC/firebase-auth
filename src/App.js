fa39a26a627b96ec1db78019f02785bcc6901f33
AxzelBC <alejo13580@gmail.com>
2022-03-08 16:31:28 -0500

    Ejemplo de APIREST.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Usuarios from './components/Usuarios'
+import Usuario from './components/Usuario'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Link to='/'>Usuarios</Link>
+        
+
+        <Routes>
+          <Route path="/" element={<Usuarios/>}/>
+          <Route path="/usuario/:id" element={<Usuario/>}/>
+        </Routes>
+      </Router>
   );
 }
 
